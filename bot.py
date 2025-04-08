e5bd4e2a0d2d54726344bf0a8ea5901ac30e4633
explicit7x <sonianuj044@gmail.com>
2025-04-08 22:24:20 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+from pyrogram import Client, filters
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH"))
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+app = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+@app.on_message(filters.command("start"))
+def start(client, message):
+    message.reply_text("Bot is working!")
+
+app.run()
